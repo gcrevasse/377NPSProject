548272a544c931585a6f1770bbd74e94e9269b2f
gcrevasse <96567716+gcrevasse@users.noreply.github.com>
2022-12-12 13:53:13 -0500

    adding page

--- script.js
+++ script.js
@@ -207,53 +207,7 @@ async function mainEvent() {
       markerPlace(currentList, pageMap);
     });
 
-    document.querySelector('.next') // Get the appropriate element (<button class="next">)
-    .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-      console.log('clicked next'); // let's tell the client console we made it to this point in the script
-      moveToNextSlide(); // call the function above to handle this
-    });
-  
-  document.querySelector('.prev')
-    .addEventListener('click', () => {
-      console.log('clicked prev');
-      moveToPrevSlide();
-    });
-    let slidePosition = 0;
-
-const slides = document.querySelectorAll('.carousel_item');
-console.log(slides, ' THIS SHOULD BE THE SLIDES')
-const slidesArray = Array.from(slides);
-
-
-const totalSlides = slidesArray.length;
-
-function updateSlidePosition() {
-  slidesArray.forEach((slide) => {
-    slide.classList.remove('visible');
-    slide.classList.add('hidden');
-  });
-  console.log(slidePosition);
-  slides[slidePosition].classList.add('visible');
- 
-}
-
-function moveToNextSlide() {
-  if (slidePosition === totalSlides - 1) {
-    slidePosition = 0;
-  } else {
-    slidePosition += 1;
-  }
-
-  updateSlidePosition(); 
-}
-function moveToPrevSlide() {
-  if (slidePosition === 0) {
-    slidePosition = totalSlides - 1;
-  } else {
-    slidePosition -= 1;
-  }
-  updateSlidePosition();
-}
+   
   }
   
 
